dd602fcd1be05e2de392086b4254664b36072246
Eduardo Rosales <edutato2008@gmail.com>
2018-09-02 17:04:03 -0600

    #7 Install React on Rails.

--- app/config/environments/development.rb
+++ app/config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  # Verifies that versions and hashed value of the package contents in the project's package.json
+  config.webpacker.check_yarn_integrity = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   #config.web_console.whitelisted_ips = ENV['DOCKER_HOST_IP']
