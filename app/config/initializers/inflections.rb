6bf75cbad60deeaa53056cccd5d7e96d6eefa164
Eduardo Rosales <edutato2008@gmail.com>
2018-09-06 01:01:43 -0600

    Fixes #10. Grammar improvements

--- app/config/initializers/inflections.rb
+++ app/config/initializers/inflections.rb
@@ -14,3 +14,19 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+ActiveSupport::Inflector.inflections(:es) do |inflect|
+    inflect.plural /$/, 's'
+    inflect.plural /([^aeioué])$/, 's'
+    inflect.plural /([aeiou]s)$/, '\1'
+    inflect.plural /z$/, 'ces'
+    inflect.plural /á([sn])$/, 'a\1es'
+    inflect.plural /í([sn])$/, 'i\1es'
+    inflect.plural /ó([sn])$/, 'o\1es'
+    inflect.plural /ú([sn])$/, 'u\1es'
+    inflect.singular /s$/, ''
+    inflect.singular /es$/, ''
+    inflect.irregular('papá', 'papás')
+    inflect.irregular('mamá', 'mamás')
+    inflect.irregular('sofá', 'sofás')
+end
